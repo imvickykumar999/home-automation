530944a8868735e1527b4d85d7fbd09ba2f07c61
imvickykumar999 <imvickykumar999@gmail.com>
2021-05-07 00:03:30 +0530

    firebase

--- PYPI/setup.py
+++ PYPI/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'imvickykumar999', # while installing pacakge, change name to something unique on pypi.org
-    version = '0.0.7', # use different version if updated, like '0.0.2'
+    version = '0.0.8', # use different version if updated, like '0.0.2'
     description = 'https://github.com/imvickykumar999/home-automation',
     long_description = open('README.md').read(),
     url = 'https://imvickykumar999.herokuapp.com/',
