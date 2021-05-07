97d8707e73cfa8fa11c0549ebae0bcf260c60871
imvickykumar999 <imvickykumar999@gmail.com>
2021-05-07 13:34:21 +0530

    folder shifting

--- Firebase/PYPI/multivicks/crud.py
+++ Firebase/PYPI/multivicks/crud.py
@@ -3,7 +3,7 @@
 # C:\Users\Vicky\anaconda3\Lib\site-packages\vicksbase
 
 class HomeAutomation:
-    def __init__(self, link):
+    def __init__(self, link = 'https://home-automation-336c0-default-rtdb.firebaseio.com/'):
 
         try:
             from vicksbase import firebase as f
@@ -11,6 +11,7 @@ class HomeAutomation:
             self.firebase_obj = f.FirebaseApplication(self.link, None)
             print(self.pull(child = '/'))
             print('''
+
 from multivicks.crud import HomeAutomation
 link = 'https://home-automation-336c0-default-rtdb.firebaseio.com/'
 obj = HomeAutomation(link)
@@ -25,7 +26,9 @@ print(f)
 
         except Exception as e:
             print(e)
+            print('Run these 2 command on cmd...')
             print('pip install --upgrade imvickykumar999')
+            print('pip uninstall firebase')
 
     def show(self):
         return self.link
